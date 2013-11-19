fc879d346ccb1044f4fef7fd2718d35c9d961377
Dermot Haughey <dermot.haughey@rvibe.com>
2013-11-19 17:13:25 -0600

    Remove vendor folder and any dependency on it

--- lib/memodis.rb
+++ lib/memodis.rb
@@ -4,14 +4,5 @@ require 'pathname'
 
 
 module Memodis
-
-  # slurp cool vendor goodies into our namespace. would declare them 
-  # as gem dependencies, but they don't seem to be published...
-
-  vendor_path = Pathname.new(__FILE__).parent.parent + 'vendor'
-  class_eval((vendor_path+'memoizable.rb').read) unless defined? Memodis::Memoizable
-  class_eval((vendor_path+'weak_cache.rb').read) unless defined? Memodis::WeakCache
-
-  include Memodis::Memoizable
-
+  include Memoizable
 end
