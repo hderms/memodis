7bbb99657a72135fcc43e4ae91a1adbf03316076
Dermot Haughey <dermot.haughey@rvibe.com>
2013-11-20 12:10:14 -0600

    Add redis set semantics

--- lib/memodis/dist_cache.rb
+++ lib/memodis/dist_cache.rb
@@ -1,4 +1,6 @@
 require 'redis'
+MEMODIS_KEYS_SET = "MEMODIS_KEYS"
+MEMODIS_KEYS_SET_EXPIRATION = 604800
 module Memodis
   class DistCache
 
@@ -19,12 +21,20 @@ module Memodis
       @decoder = resolve_coder(options[:decoder])
       @key_gen = options.fetch(:key_gen, lambda { |k| k })
       @expires = options[:expires]
+      set_keys_set_expiration unless keys_set_exists?
     end
 
     def []= key, val
       key = canonicalize(key)
       @master.set(key, encode(val))
+      @master.sadd(MEMODIS_KEYS_SET, key)
       @master.expire(key, @expires) unless @expires.nil?
+    end
+    
+    def del key
+      key = canonicalize(key)
+      @master.srem(MEMODIS_KEYS_SET, key)
+      @master.del(key)
     end
 
     def [] key
@@ -43,8 +53,20 @@ module Memodis
     end
 
 
+    def set_keys_set_expiration
+      @master.expire MEMODIS_KEYS_SET, MEMODIS_KEYS_SET_EXPIRATION
+    end
+    def keys_set_exists?
+      @master.exists MEMODIS_KEYS_SET
+    end
+    def in_keys_set?
+      @master.sismember(MEMODIS_KEYS_SET, key)
+      end
+
     def get key
       # TODO log warning if s_node == m_node
+      return nil unless in_keys_set?(key)
+
       @master.get(key)
     end
 
